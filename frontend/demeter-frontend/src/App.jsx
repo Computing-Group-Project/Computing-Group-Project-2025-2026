068b9fb5d6e570f839c873d9046aca96b1867449
sdk354 <sdkalubowila@students.nsbm.ac.lk>
2026-03-05 13:48:58 +0530

    Unfinished changes to frontend checkout package (#41)
    * Unfinished changes to frontend checkout package
    
    # Conflicts:
    #	frontend/demeter-frontend/package-lock.json
    #	frontend/demeter-frontend/src/App.jsx
    
    * Add checkout flow implementation
    
    ---------
    
    Co-authored-by: Asmarafee <asmarafee07@gmail.com>

--- frontend/demeter-frontend/src/App.jsx
+++ frontend/demeter-frontend/src/App.jsx
@@ -1,10 +1,28 @@
-import StudentLayout from "./layouts/StudentLayout";
-import StudentHome from "./student/StudentHome";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import StaffLayout from "./layouts/StaffLayout";
+import AdminConsole from "./admin/AdminConsole";
+import StaffDashboard from "./staff/StaffDashboard";
+import Checkout from "./student/Checkout";
 
-export default function App() {
-  return (
-    <StudentLayout>
-      <StudentHome />
-    </StudentLayout>
-  );
+function App() {
+    return (
+        <ThemeProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/admin" element={<AdminConsole />} />
+                    <Route path="/" element={<Checkout />} />
+                    <Route path="/staff" element={
+                        <StaffLayout>
+                            <StaffDashboard />
+                        </StaffLayout>} />
+                    <Route path="/checkout" element={<Checkout />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
+
+export default App;
+//changed the Structure of the previous code and fixed some errors
