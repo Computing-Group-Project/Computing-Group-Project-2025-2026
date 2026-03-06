aefef49318da499dd03edcb15fd7a0eb2f090578
DuminduDK <ddkoralearachchi@students.nsbm.ac.lk>
2026-03-06 11:36:18 +0530

    feat(frontend): add cart and orders UI pages

--- frontend/demeter-frontend/src/App.jsx
+++ frontend/demeter-frontend/src/App.jsx
@@ -1,20 +1,11 @@
 import { ThemeProvider } from './contexts/ThemeContext';
-import AdminConsole from './admin/AdminConsole';
+import Orders from './student/Orders';
 
 function App() {
   return (
     <ThemeProvider>
-      <AdminConsole />
+      <Orders />
     </ThemeProvider>
-import React from 'react';
-import StaffDashboard from './staff/StaffDashboard';
-import StaffLayout from './layouts/StaffLayout';
-
-function App() {
-  return (
-    <StaffLayout>
-      <StaffDashboard/>
-    </StaffLayout>
   );
 }
 
